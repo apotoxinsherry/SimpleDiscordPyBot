ef878c1438c94787f961a103af39e0db6872389f
apotoxinsherry <anirudhganapathy2003@gmail.com>
2022-05-12 23:20:45 +0530

    added the Manga command

--- Cogs/anilist.py
+++ Cogs/anilist.py
@@ -27,6 +27,17 @@ class anidata(commands.Cog):
         my_embed.add_field(name='Genres', value=animedata['genres'], inline=False)
         await ctx.send(embed=my_embed)
 
+    @commands.command()
+    async def manga(self, ctx, name):
+        mangadata = my_client.get_manga(name)
+        mangaid = my_client.get_manga_id(name)
+        my_embed = discord.Embed(title=mangadata['name_english'], url=f"https://anilist.co/anime/{mangaid}", description=mangadata['desc'], color=0x323834)
+        my_embed.set_thumbnail(url=mangadata['cover_image'])
+        my_embed.add_field(name='Chapters', value=mangadata['chapters'], inline=True)
+        my_embed.add_field(name="Rating", value=mangadata['average_score'], inline=True)
+        my_embed.add_field(name='Genres', value=mangadata['genres'], inline=False)
+        await ctx.send(embed=my_embed)
+
 
 
 def setup(client):
