2c0f0aeb2dfc44c67f3de3d39818aab57b12ad85
apotoxinsherry <anirudhganapathy2003@gmail.com>
2022-06-03 14:35:46 +0530

    implemented half the logic

--- Cogs/tictactoe.py
+++ Cogs/tictactoe.py
@@ -3,13 +3,45 @@ from nextcord.ext import commands
 from random import randint
 
 #to-do:
+#ensure that misc input by the user don't get in the way of the game
 #implement a users-in-game function which prevernts already existing users from re-running the command again. 
 #implement the actual game logic
-#create a tic-tac-toe player class for each player, which keeps track of each player's data
-#this new class is inherited by or an instance of the class is declared within the class which has commands.Cog
 
 
-board = ['','X', 'X', 'O', 'O', 'X', 'X', 'O', 'O', 'X']
+#general game logic:
+#player calls for the game by using the playttt function and the ingame flag is set to true -- this is checked by checking if the player is on the players list.
+# it checks if the command has already been run 
+#also maybe create a function to obtain and verify user input
+# the event on_message executes here only if ingame so as to avoid unnecessary triggers
+# on_message then gets the symbol for the user
+
+
+
+
+class player():
+    def __init__(self, name):
+        self.name = name
+        self.uchoice = 'X'
+        self.cchoice = 'O'
+        self.gameboard = [' ']*10
+    
+
+    def display(self):
+        return f'''
+{self.gameboard[7]} | {self.gameboard[8]} | {self.gameboard[9]}
+--|---|----
+{self.gameboard[4]} | {self.gameboard[5]} | {self.gameboard[6]}
+--|---|----
+{self.gameboard[1]} | {self.gameboard[2]} | {self.gameboard[3]}'''
+
+
+
+
+
+
+
+
+
 
 def checker(icon, board):
     if board[1]==board[2]==board[3]==icon:
@@ -33,95 +65,63 @@ def checker(icon, board):
 
 
 
-def clearboard():
-    global board
-    board = [""]*10
 
 
 def inputverify(input):
-    return input in range(1,10)
+    return input in [x for x in range(1,10)]
 
 
 
 class tictactoe(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-        self.choices = ['X', 'O']
-        self.pchoice = ''
-        self.cchoice = ''
-        self.board = f'''
-{board[7]} | {board[8]} | {board[9]}
----|------
-{board[4]} | {board[5]} | {board[6]}
---|---|----
-{board[1]} | {board[2]} | {board[3]}'''
+        self.players = []
+        
 
 
-    def initialize(self):
-        clearboard()
-
-    async def drawboard(self, ctx): 
-        await ctx.send(self.board)
-
+    async def gameplay(self, ctx,player):
+        await ctx.send("Provide a number from 1 to 9")
+        ingame = True
+        inp = await self.bot.wait_for('message')
+        correctInput =  inputverify(int(inp.content))
+        while not correctInput:
+            await ctx.send("Please enter a valid location")
+            inp = await self.bot.wait_for('message')
+            correctInput = inputverify(inp.content)
+        else:
+            ingame = True
+            while ingame:
+                await ctx.send(player.display())
+                kek = await self.bot.wait_for('message')
+                await ctx.send(kek)
+                print("end of operation")
+                break
     
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        inp = await self.bot.wait_for('message')
-        print(inp)
 
 
-    async def gameplay(self, ctx):
-        await ctx.send("Provide a number from 1 to 9")
-        inp = await ctx.wait_for('message')
-        correctInput = inputverify(inp)
-        if correctInput:
-            await ctx.send(inp)
-        else:
-            await ctx.send("Please provide a valid number")
+
+
+
+    def alreadyPlaying(self,user):
+        for item in self.players:
+            if item.name == user:
+                return True
+        return False
+
+
+
 
 
     @commands.command()
-    async def playttt(self, ctx, choice):
-        self.player = ctx.author
-        self.inGame = True
-        if choice.capitalize() not in self.choices:
-            await ctx.send("Please provide a valid choice and re-run the command")
+    async def playttt(self, ctx):
+        if not self.alreadyPlaying(ctx.author):
+            self.players.append(player(ctx.author))
+            await ctx.send(self.players[-1])
+            await self.gameplay(ctx=ctx, player=self.players[-1])
+
         else:
-            self.pchoice = choice.capitalize()
-            self.choices.remove(self.pchoice)
-            self.cchoice = self.choices[0]
-            await self.drawboard(ctx=ctx)
-            await ctx.send(f"User Choice: {self.pchoice}")
-            await ctx.send(f"Computer Choice: {self.cchoice}") 
-            await self.gameplay()
-        
-
-    
-    
-
-
-    
-
-    @playttt.error
-    async def playttt_error(self, ctx, error):
-        await ctx.send("Please provide a kek")
-
-
-
-
-    
-
-
-        
-
-
-
-
-
-
-
-
+            await ctx.send("Sorry, this user is already in game")
 
 
 
